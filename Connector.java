6b20cd36a5fb3bf94c273e6f2141a3a43beefcd0
Hari Govind K K <45679986+harigovindkk@users.noreply.github.com>
2021-01-10 11:18:01 +0530

    Java Files Added

---
+++ Connector.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asdtemp;
+
+/**
+ *
+ * @author harig
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Connector {
+     Connection con=null;
+    public Connector() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con=DriverManager.getConnection("jdbc:mysql://localhost/?user=root&password=root");
+    }
+    Connection getCon() {
+        return con;
+    }
+}
